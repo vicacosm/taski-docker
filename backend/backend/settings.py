d526648a6b5b7b22cff9157f26e1043b41af9feb
vicacosm <vicacos@mail.ru>
2024-10-20 16:06:41 +0300

    requiremets

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['158.160.86.2', 'https://victory.hopto.org/', 'victory.hopto.org', 'localhost', '127.0.0.1']
 
 
 # Application definition
